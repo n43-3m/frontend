7c0ab675c5fe5b8e1b7bd8e4f8dd11ff85c2c084
n43-3m <naeemur006@gmail.com>
2025-10-13 00:12:53 +0600

    add husky and lint-staged

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// biome-ignore lint: root will always be there in the index.html
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
 	</StrictMode>
 )
-
